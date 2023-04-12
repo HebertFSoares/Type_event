855d7ecfa2a4bb94b6a14d9a83059c066fb2f49c
Hebert Soares <hebertsoaresof@gmail.com>
2023-04-12 20:44:51 -0300

    Criação do models do novo_evento

--- eventos/views.py
+++ eventos/views.py
@@ -6,3 +6,5 @@ from django.contrib.auth.decorators import login_required
 def novo_evento(request):
     if request.method == "GET":
         return render(request, 'novo_evento.html')
+    elif request.method == "POST":
+        
