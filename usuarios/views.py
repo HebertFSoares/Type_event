b409fbce64b0e35cd268686947eedd0a84f5ccba
Hebert Soares <hebertsoaresof@gmail.com>
2023-04-12 13:34:51 -0300

    Finalização do template de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirma_senha = request.POST.get('confirmar_senha')
+       
