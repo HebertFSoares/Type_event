c80e0108ef9b914c7b632732933972b0d5839dff
Hebert Soares <hebertsoaresof@gmail.com>
2023-04-12 13:57:41 -0300

    Fazendo verificações do cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
+import re
+
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
@@ -10,4 +12,18 @@ def cadastro(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirma_senha = request.POST.get('confirmar_senha')
-       
+        
+        if len(senha) < 7:
+           return redirect('/usuarios/cadastro')
+            
+        if not senha == confirma_senha:
+            return redirect('/usuarios/cadastro')
+        
+        user  = User.objects.filter(username=username)
+        
+        if user.exists():
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.create_user(username=username,email=email,password=senha)
+        
+        return HttpResponse("CERTO")
