0cc17e4c019d2c3f547f8e6f84a0d57683213d7c
Hebert Soares <hebertsoaresof@gmail.com>
2023-04-12 20:31:17 -0300

    Criação da tela novo_evento

--- eventos/views.py
+++ eventos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def novo_evento(request):
+    if request.method == "GET":
+        return render(request, 'novo_evento.html')
