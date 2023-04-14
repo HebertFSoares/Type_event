92d0c237c62ed155b58e29584a53b7cd0245143e
Hebert Soares <hebertsoaresof@gmail.com>
2023-04-14 11:11:39 -0300

    Criação das views do novo_evento

--- eventos/models.py
+++ eventos/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Evento(models.Model):
+    criador = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
     data_inicio = models.DateField()
     data_termino = models.DateField()
-    carga_horario = models.IntegerField()
+    carga_horaria = models.IntegerField()
     logo = models.ImageField(upload_to="logos")
     
     #paleta de cores
